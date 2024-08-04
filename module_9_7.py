089fabec3d53ed44b61c6fc90939b831883c7c63
Igor Dm <gid68@mail.ru>
2024-08-04 22:21:05 +0300

    Add files via upload

---
+++ module_9_7.py
@@ -0,0 +1,27 @@
+# "Декораторы"
+def is_prime(func):
+    def inner(*args, **kwargs):
+        x = func(*args, **kwargs)
+        for i in range(2, int(x ** 0.5 + 1)):
+            if x % i == 0:
+                print("Составное")
+                break
+        else:
+            if x > 1:
+                print("Простое")
+        return x
+
+    return inner
+
+
+@is_prime
+def sum_three(one, two, three):
+    return sum((one, two, three))
+
+
+result = sum_three(5, 0, 0)
+print(result)
+
+# Результат консоли:
+# Простое
+# 11
